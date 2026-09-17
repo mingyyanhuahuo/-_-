7a0dacebbbc80d8cc4444395b98eb35a3a702689
scp2733wx <is.Wangxin@outlook.com>
2026-09-17 16:08:50 +0800

    fix(frequentware): 补全错误检测
    Changes to be committed:
    	modified:   middleware/frequentware.go

--- middleware/frequentware.go
+++ middleware/frequentware.go
@@ -1,9 +1,12 @@
 package middleware
 
 import (
+	"lostfound/pkg/logger"
 	"lostfound/pkg/redisdb"
+	"lostfound/pkg/response"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func FrequentWare() gin.HandlerFunc {
@@ -11,10 +14,14 @@ func FrequentWare() gin.HandlerFunc {
 		ip := c.ClientIP()
 		err, ErrCode := redisdb.RequestsTimeLimit(ip)
 		if err != nil {
-
+			logger.Logger.Error("未知错误", zap.Error(err))
+			c.Abort()
+			return
 		}
 		if ErrCode != nil {
-
+			response.Err(c, ErrCode)
+			c.Abort()
+			return
 		}
 	}
 }
